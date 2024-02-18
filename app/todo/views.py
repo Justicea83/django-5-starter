1a493876bfef028f03d97fc884abb3c97f933fc3
Justice Arthur <justicea83@gmail.com>
2024-02-18 16:53:49 -0500

    setup redis server and little cache

--- app/todo/views.py
+++ app/todo/views.py
@@ -1,4 +1,6 @@
 from rest_framework import mixins, viewsets
+from django.core.cache import cache
+from rest_framework.response import Response
 
 from core.models import Todo
 from todo.serializers import TodoSerializer
@@ -18,3 +20,19 @@ class TodoViewSet(
 
     def perform_create(self, serializer):
         serializer.save()
+
+    def list(self, request, *args, **kwargs):
+        cache_key = 'todo_list'
+        data = cache.get(cache_key)
+        if not data:
+            queryset = self.filter_queryset(self.get_queryset())
+
+            page = self.paginate_queryset(queryset)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+
+            serializer = self.get_serializer(queryset, many=True)
+            data = serializer.data
+            cache.set(cache_key, serializer.data, 60 * 15)
+        return Response(data)
